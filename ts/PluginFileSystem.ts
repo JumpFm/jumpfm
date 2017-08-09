6615d1de8f300ba1f637621254eb05765dd5f51d
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-09 09:37:00 +0300

    ~76

--- ts/PluginFileSystem.ts
+++ ts/PluginFileSystem.ts
@@ -2,6 +2,7 @@ import { Plugin } from './Plugin'
 import { JumpFm } from './JumpFm'
 import { Panel } from './Panel'
 import { Item } from './Item'
+import { opn } from './opn'
 
 import * as homedir from 'homedir'
 import * as fs from 'fs'
@@ -15,11 +16,15 @@ class PluginFileSystem extends Plugin {
     }
 
     cd = (panel: Panel, url: string, info: any) => {
+        if (!fs.existsSync(url)) return
+        if (fs.statSync(url).isDirectory) this.ll(panel, url)
+        else opn(url)
+    }
+
+    ll = (panel, url) => {
         // TODO add watcher
-        // TODO check if url is dir
         panel.setItems(fs.readdirSync(url).map((name): Item => {
             const fullPath = path.join(url, name)
-            // TODO check if file exists
             const stat = fs.statSync(fullPath)
             return {
                 icon: 'file-icons/default_file.svg',
