9f0b8f4f09a0b0f1e141f26aab819a28df4c9409
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-09 21:15:10 +0300

    ~70

---
+++ ts/PluginFileOperations.ts
@@ -0,0 +1,33 @@
+import { JumpFm } from './JumpFm'
+import { Plugin } from './Plugin'
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+class PluginFileOperations extends Plugin {
+    onLoad(): void {
+        const jumpFm = this.jumpFm
+        const dialog = jumpFm.dialog
+        const bind = this.jumpFm.bindKeys
+        const activePanel = () => jumpFm.panels.getActivePanel()
+
+        bind('rename', ['f2'], () => {
+            dialog.open({
+                label: 'Rename',
+                onOpen: input => {
+                    input.value = activePanel().getCurItem().name
+                    input.select()
+                },
+                onAccept: name => {
+                    const pan = activePanel()
+                    fs.renameSync(
+                        pan.getCurItem().url,
+                        path.join(pan.getUrl(), name)
+                    )
+                },
+            })
+        })
+    }
+}
+
+module.exports = PluginFileOperations
