779dbb6f2d0801dfa5c2449a56562f5f35fcc079
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-11 07:42:42 +0300

    flat mode

---
+++ ts/PluginFlatMode.ts
@@ -0,0 +1,64 @@
+import { JumpFm } from './JumpFm'
+import { Plugin } from './Plugin'
+import { Panel } from './Panel'
+import { Item, itemFromPath } from './Item'
+
+import * as watch from 'node-watch'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const MAX_SIZE = 200
+
+class Flat {
+    watcher: { close: () => {} }
+
+    constructor(jumpFm: JumpFm, panel: Panel) {
+        panel.onCd(url => {
+            if (url.protocol != 'flat') return
+            const items = this.flat(url.path)
+            panel.setItems(items)
+            if (items.length > MAX_SIZE)
+                jumpFm.statusBar.msg(
+                    'flat',
+                    'Flat Mode: too many files to show',
+                    ['err'],
+                    2000
+                )
+        })
+    }
+
+    private flat = (dir): Item[] => {
+        function flatDir(rootDir: string, res: Item[]) {
+            if (res.length > MAX_SIZE) return
+
+            fs.readdirSync(rootDir)
+                .map(name => path.join(rootDir, name))
+                .filter(fullPath => fs.existsSync(fullPath))
+                .forEach(fullPath => {
+                    const stat = fs.statSync(fullPath)
+                    if (stat.isDirectory()) flatDir(fullPath, res)
+                    else res.length <= MAX_SIZE && res.push(itemFromPath(fullPath))
+                })
+        }
+
+        const res = []
+        flatDir(dir, res)
+        return res
+    }
+}
+
+class PluginFlatMode extends Plugin {
+    onLoad(): void {
+        const jumpFm = this.jumpFm
+        jumpFm.panels.forEach(panel => new Flat(jumpFm, panel))
+        jumpFm.bindKeys('flatMode', ['r'], () => {
+            const pan = jumpFm.getActivePanel()
+            const url = pan.getUrl()
+            if (url.protocol == 'flat') url.protocol = ''
+            else url.protocol = 'flat'
+            pan.cd(url)
+        })
+    }
+}
+
+module.exports = PluginFlatMode
